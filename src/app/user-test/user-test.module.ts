6958c51d436fd7461fa5d2d4f101951e4e8dc3c1
Ashishchaursiya <ashishc@codewave.com>
2023-01-15 22:20:03 +0530

    create two modules.

---
+++ src/app/user-test/user-test.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserTestRoutingModule } from './user-test-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserTestRoutingModule
+  ]
+})
+export class UserTestModule { }
